4969bfc39f33ec8f5d39f2f9c4d9a9a0632675a9
rkr-abs <rkr@abstractit.in>
2023-08-01 10:29:01 +0530

    Initialized

---
+++ main.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+name = input("Enter your name: ")
+txt = "Welcome to the Python Starter Repository! 🎉 ✨ Welcome aboard, {name}! 🐍💻 Kickstart your Python journey with us Happy coding! 🎉"
+print(txt.format(name=name))
