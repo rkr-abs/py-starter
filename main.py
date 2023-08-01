44ee54a9b73067b0bff0bdb7c8995b9808f9dc8d
rkr-abs <rkr@abstractit.in>
2023-08-01 10:52:49 +0530

    Introduced editor config.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 name = input("Enter your name: ")
 txt = "Welcome to the Python Starter Repository! 🎉 ✨ Welcome aboard, {name}! 🐍💻 Kickstart your Python journey with us Happy coding! 🎉"
 print(txt.format(name=name))
